477adef11f4f73c2752226a8fe324bfa01b39a33
s1290154 <s1290154@u-aizu.ac.jp>
2021-07-30 13:47:59 +0900

    localstrage機能を追加

--- ex4.js
+++ ex4.js
@@ -1,20 +1,58 @@
-const Inputtask = document.getElementById("inputtask")
-const Addtask = document.getElementById("addtask")
-const Tasks = document.getElementById("tasks")
+const inputTask = document.getElementById("inputtask")
+const addTask = document.getElementById("addtask")
+const taskListContainer = document.getElementById("tasklistcontainer")
+let tasks = []
 
-Addtask.addEventListener("click",() => {
-    const todo = document.createElement("div")
-    const task = document.createElement("p")
-    const remove = document.createElement("input")
-
-    remove.type = "button"
-    remove.value = "削除"
-    remove.addEventListener("click",() => todo.remove())
-    task.textContent = Inputtask.value
-
-    todo.appendChild(task)
-    todo.appendChild(remove)
-    Tasks.appendChild(todo)
-
-    Inputtask.value = ""
+addTask.addEventListener("click", () => {
+    let newTaskId
+    if(tasks.length !== 0){
+        newTaskId = tasks.slice(-1)[0].id + 1
+    }else{
+        newTaskId = 1
+    }
+    const task = {
+        id: newTaskId,
+        text: inputTask.value
+    }
+    tasks.push(task)
+    const tasksJson = JSON.stringify(tasks)
+    localStorage.removeItem("hoge")
+    localStorage.setItem("hoge",tasksJson)
+    update()
+    inputTask.value = ""
 })
+
+const update = () => {
+    while(taskListContainer.firstChild !== null){
+        taskListContainer.removeChild(taskListContainer.firstChild)
+    }
+    tasks = JSON.parse(localStorage.getItem("hoge"))
+    if(tasks === null){
+        tasks = []
+    }
+    for(let i = 0; i < tasks.length; i++){
+        const task = tasks[i]
+        const taskscontainer = document.createElement("div")
+        taskscontainer.id = task.id
+        const taskp = document.createElement("p")
+        taskp.textContent = task.text
+
+        const remove = document.createElement("input")
+        remove.type = "button"
+        remove.value = "削除"
+        remove.addEventListener("click", () => {
+            localStorage.removeItem("hoge")
+            if(tasks[i]!==null){
+                tasks.splice(i,1)
+                const tasksJson = JSON.stringify(tasks)
+                localStorage.setItem("hoge",tasksJson)
+                update()
+            }
+        })
+        taskscontainer.appendChild(taskp)
+        taskscontainer.appendChild(remove)
+        taskListContainer.appendChild(taskscontainer)
+    }
+}
+
+update()
